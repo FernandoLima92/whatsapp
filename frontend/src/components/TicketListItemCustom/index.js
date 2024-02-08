ecd1659b5aa77f39cd3426c2a4b9c4826a402af9
Matheus Araujo <matheus.kyuubinoz@gmail.com>
2024-02-08 00:18:33 -0300

    missão cumprida

--- frontend/src/components/TicketListItemCustom/index.js
+++ frontend/src/components/TicketListItemCustom/index.js
@@ -183,87 +183,190 @@ const useStyles = makeStyles((theme) => ({
 
   }
 }));
-
-const TicketListItemCustom = ({ ticket }) => {
+  {/*PLW DESIGN INSERIDO O dentro do const handleChangeTab*/}
+  const TicketListItemCustom = ({ ticket }) => {
   const classes = useStyles();
   const history = useHistory();
-  const { ticketId } = useParams();
   const [loading, setLoading] = useState(false);
+  const [ticketUser, setTicketUser] = useState(null);
+  const [ticketQueueName, setTicketQueueName] = useState(null);
+  const [ticketQueueColor, setTicketQueueColor] = useState(null);
+  const [tag, setTag] = useState([]);
+  const [whatsAppName, setWhatsAppName] = useState(null);
+
+  const [openTicketMessageDialog, setOpenTicketMessageDialog] = useState(false);
+  const { ticketId } = useParams();
   const isMounted = useRef(true);
+  const { setCurrentTicket } = useContext(TicketsContext);
   const { user } = useContext(AuthContext);
+  const { profile } = user;
 
   useEffect(() => {
+    if (ticket.userId && ticket.user) {
+      setTicketUser(ticket.user?.name?.toUpperCase());
+    }
+    setTicketQueueName(ticket.queue?.name?.toUpperCase());
+    setTicketQueueColor(ticket.queue?.color);
+
+    if (ticket.whatsappId && ticket.whatsapp) {
+      setWhatsAppName(ticket.whatsapp.name?.toUpperCase());
+    }
+
+    setTag(ticket?.tags);
+
     return () => {
       isMounted.current = false;
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleAcceptTicket = async (id) => {
+  {/*CÓDIGO NOVO SAUDAÇÃO*/}
+  const handleCloseTicket = async (id) => {
+    setTag(ticket?.tags);
+    setLoading(true);
+    try {
+      await api.put(`/tickets/${id}`, {
+        status: "closed",
+        userId: user?.id,
+        queueId: ticket?.queue?.id
+      });
+    } catch (err) {
+      setLoading(false);
+      toastError(err);
+    }
+    if (isMounted.current) {
+      setLoading(false);
+    }
+    history.push(`/tickets/`);
+  };
+
+  const handleReopenTicket = async (id) => {
     setLoading(true);
     try {
       await api.put(`/tickets/${id}`, {
         status: "open",
         userId: user?.id,
+        queueId: ticket?.queue?.id
       });
-      // if (isMounted.current) {
-      //   setLoading(false);
-      //   history.push(`/tickets/${ticket.uuid}`);
-      // }
-
-      let settingIndex;
-
-      try {
-          const { data } = await api.get("/settings/");
-          settingIndex = data.filter((s) => s.key === "sendGreetingAccepted");
-      } catch (err) {
-          toastError(err);
-      }
-
-      if (settingIndex[0].value === "enabled" && !ticket.isGroup) {
-          handleSendMessage(ticket.id);
-      }
     } catch (err) {
-      if (isMounted.current) {
-        setLoading(false);
+      setLoading(false);
+      toastError(err);
+    }
+    if (isMounted.current) {
+      setLoading(false);
+    }
+    history.push(`/tickets/${ticket.uuid}`);
+  };
+
+    const handleAcepptTicket = async (id) => {
+        setLoading(true);
+        try {
+            await api.put(`/tickets/${id}`, {
+                status: "open",
+                userId: user?.id,
+            });
+
+            let settingIndex;
+
+            try {
+                const { data } = await api.get("/settings/");
+                settingIndex = data.filter((s) => s.key === "sendGreetingAccepted");
+            } catch (err) {
+                toastError(err);
+            }
+
+            if (settingIndex[0].value === "enabled" && !ticket.isGroup) {
+                handleSendMessage(ticket.id);
+            }
+
+        } catch (err) {
+            setLoading(false);
+            toastError(err);
+        }
+        if (isMounted.current) {
+            setLoading(false);
+        }
+
+        // handleChangeTab(null, "tickets");
+        // handleChangeTab(null, "open");
         history.push(`/tickets/${ticket.uuid}`);
-      }
+    };
+	
+	    const handleSendMessage = async (id) => {
+        const msg = `{{ms}} *{{name}}*, meu nome é *${user?.name}* e agora vou prosseguir com seu atendimento!`;
+        const message = {
+            read: 1,
+            fromMe: true,
+            mediaUrl: "",
+            body: `*Mensagem Automática:*\n${msg.trim()}`,
+        };
+        try {
+            await api.post(`/messages/${id}`, message);
+        } catch (err) {
+            toastError(err);
+        }
+    };
+	{/*CÓDIGO NOVO SAUDAÇÃO*/}
+
+  const handleSelectTicket = (ticket) => {
+    const code = uuidv4();
+    const { id, uuid } = ticket;
+    setCurrentTicket({ id, uuid, code });
+  };
+
+
+  const renderTicketInfo = () => {
+    if (ticketUser) {
+
+      return (
+        <>
+          {ticket.chatbot && (
+            <Tooltip title="Chatbot">
+              <AndroidIcon
+                fontSize="small"
+                style={{ color: grey[700], marginRight: 5 }}
+              />
+            </Tooltip>
+          )}
+
+          {/* </span> */}
+        </>
+      );
+    } else {
+      return (
+        <>
+          {ticket.chatbot && (
+            <Tooltip title="Chatbot">
+              <AndroidIcon
+                fontSize="small"
+                style={{ color: grey[700], marginRight: 5 }}
+              />
+            </Tooltip>
+          )}
+        </>
+      );
     }
   };
 
-  const handleSendMessage = async (id) => {
-    const msg = `{{ms}} *{{name}}*, meu nome é *${user?.name}* e agora vou prosseguir com seu atendimento!`;
-    const message = {
-        read: 1,
-        fromMe: true,
-        mediaUrl: "",
-        body: `*Mensagem Automática:*\n${msg.trim()}`,
-    };
-    try {
-        await api.post(`/messages/${id}`, message);
-    } catch (err) {
-        toastError(err);
-    }
-};
-
   return (
     <React.Fragment key={ticket.id}>
-      {/* <TicketMessagesDialog
+      <TicketMessagesDialog
         open={openTicketMessageDialog}
 
         handleClose={() => setOpenTicketMessageDialog(false)}
         ticketId={ticket.id}
-      ></TicketMessagesDialog> */}
-      <ListItem
-        dense
-        button
-        onClick={() => {
-          if (ticket.status === "pending" || !ticket.queue) return;
-          history.push(`/tickets/${ticket.uuid}`);
+      ></TicketMessagesDialog>
+      <ListItem dense button
+        onClick={(e) => {
+          if (ticket.status === "pending") return;
+          handleSelectTicket(ticket);
         }}
-        selected={ticketId === String(ticket.id)}
-        className={classes.ticket}
+        selected={ticketId && +ticketId === ticket.id}
+        className={clsx(classes.ticket, {
+          [classes.pendingTicket]: ticket.status === "pending",
+        })}
       >
-        <Tooltip title={ticket.queue?.name?.toUpperCase() || i18n.t("ticketsList.noQueue")}>
+        <Tooltip arrow placement="right" title={ticket.queue?.name?.toUpperCase() || "SEM FILA"} >
           <span style={{ backgroundColor: ticket.queue?.color || "#7C7C7C" }} className={classes.ticketQueueColor}></span>
         </Tooltip>
         <ListItemAvatar>
@@ -292,24 +395,147 @@ const TicketListItemCustom = ({ ticket }) => {
           }
         </ListItemAvatar>
         <ListItemText
-          primary={<Typography variant="body2">{ticket.contact.name}</Typography>}
+          disableTypography
+
+          primary={
+            <span className={classes.contactNameWrapper}>
+              <Typography
+                noWrap
+                component="span"
+                variant="body2"
+                color="textPrimary"
+              >
+                {ticket.contact.name}
+                {profile === "admin" && (
+                  <Tooltip title="Espiar Conversa">
+                    <VisibilityIcon
+                      onClick={() => setOpenTicketMessageDialog(true)}
+                      fontSize="small"
+                      style={{
+                        color: blue[700],
+                        cursor: "pointer",
+                        marginLeft: 10,
+                        verticalAlign: "middle"
+                      }}
+                    />
+                  </Tooltip>
+                )}
+              </Typography>
+              <ListItemSecondaryAction>
+                <Box className={classes.ticketInfo1}>{renderTicketInfo()}</Box>
+              </ListItemSecondaryAction>
+            </span>
+
+          }
+          secondary={
+            <span className={classes.contactNameWrapper}>
+
+              <Typography
+                className={classes.contactLastMessage}
+                noWrap
+                component="span"
+                variant="body2"
+                color="textSecondary"
+              > {ticket.lastMessage.includes('data:image/png;base64') ? <MarkdownWrapper> Localização</MarkdownWrapper> : <MarkdownWrapper>{ticket.lastMessage}</MarkdownWrapper>}
+                <span className={classes.secondaryContentSecond} >
+                  {ticket?.whatsapp?.name ? <Badge className={classes.connectionTag}>{ticket?.whatsapp?.name?.toUpperCase()}</Badge> : <br></br>}
+                  {ticketUser ? <Badge style={{ backgroundColor: "#000000" }} className={classes.connectionTag}>{ticketUser}</Badge> : <br></br>}
+                  <Badge style={{ backgroundColor: ticket.queue?.color || "#7c7c7c" }} className={classes.connectionTag}>{ticket.queue?.name?.toUpperCase() || "SEM FILA"}</Badge>
+                </span>
+                <span style={{ paddingTop: "2px" }} className={classes.secondaryContentSecond} >
+                  {tag?.map((tag) => {
+                    return (
+                      <ContactTag tag={tag} key={`ticket-contact-tag-${ticket.id}-${tag.id}`} />
+                    );
+                  })}
+                </span>
+              </Typography>
+
+              <Badge
+                className={classes.newMessagesCount}
+                badgeContent={ticket.unreadMessages}
+                classes={{
+                  badge: classes.badgeStyle,
+                }}
+              />
+            </span>
+          }
+
         />
-        {ticket.status === "pending" && ticket.queue && (
-          <ButtonWithSpinner
-            variant="contained"
-            color="primary"
-            className={classes.acceptButton}
-            size="small"
-            loading={loading}
-            onClick={(e) => {
-              e.stopPropagation(); // Impede que o evento de clique no ListItem seja acionado
-              handleAcceptTicket(ticket.id);
-            }}
-          >
-            {i18n.t("ticketsList.buttons.accept")}
-          </ButtonWithSpinner>
-        )}
+        <ListItemSecondaryAction>
+          {ticket.lastMessage && (
+            <>
+
+              <Typography
+                className={classes.lastMessageTime}
+                component="span"
+                variant="body2"
+                color="textSecondary"
+              >
+
+                {isSameDay(parseISO(ticket.updatedAt), new Date()) ? (
+                  <>{format(parseISO(ticket.updatedAt), "HH:mm")}</>
+                ) : (
+                  <>{format(parseISO(ticket.updatedAt), "dd/MM/yyyy")}</>
+                )}
+              </Typography>
+
+              <br />
+
+            </>
+          )}
+
+        </ListItemSecondaryAction>
+        <span className={classes.secondaryContentSecond} >
+          {ticket.status === "pending" && ticket.queue && (
+            <ButtonWithSpinner
+              //color="primary"
+              style={{ backgroundColor: 'green', color: 'white', padding: '0px', bottom: '17px', borderRadius: '0px', left: '8px', fontSize: '0.6rem' }}
+              variant="contained"
+              className={classes.acceptButton}
+              size="small"
+              loading={loading}
+			  //PLW DESIGN INSERIDO O handleChangeT ab
+        onClick={(e) => {
+          e.stopPropagation(); // Impede que o evento de clique no ListItem seja acionado
+          handleAcepptTicket(ticket.id);
+        }}
+            >
+              {i18n.t("ticketsList.buttons.accept")}
+            </ButtonWithSpinner>
+
+          )}
+          {(ticket.status !== "closed") && (
+            <ButtonWithSpinner
+              //color="primary"
+              style={{ backgroundColor: 'red', color: 'white', padding: '0px', bottom: '0px', borderRadius: '0px', left: '8px', fontSize: '0.6rem' }}
+              variant="contained"
+              className={classes.acceptButton}
+              size="small"
+              loading={loading}
+              onClick={e => handleCloseTicket(ticket.id)}
+            >
+              {i18n.t("ticketsList.buttons.closed")}
+            </ButtonWithSpinner>
+
+          )}
+          {(ticket.status === "closed") && (
+            <ButtonWithSpinner
+              //color="primary"
+              style={{ backgroundColor: 'red', color: 'white', padding: '0px', bottom: '0px', borderRadius: '0px', left: '8px', fontSize: '0.6rem' }}
+              variant="contained"
+              className={classes.acceptButton}
+              size="small"
+              loading={loading}
+              onClick={e => handleReopenTicket(ticket.id)}
+            >
+              {i18n.t("ticketsList.buttons.reopen")}
+            </ButtonWithSpinner>
+
+          )}
+        </span>
       </ListItem>
+
       <Divider variant="inset" component="li" />
     </React.Fragment>
   );
